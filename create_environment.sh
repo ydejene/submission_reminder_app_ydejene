4e63e186c320d12a76a0167eefed1d62bef30410
ydejene <y.dejene@alustudent.com>
2025-06-14 03:26:14 -0700

    sub-directories and files have been created

--- create_environment.sh
+++ create_environment.sh
@@ -7,10 +7,10 @@ DirName="submission_reminder_$Uname"
 
 #validating if Uname is empty or not
 if [[ -n "$Uname" ]]; then
-	echo "the directory '$DirName' will be created."
+	echo "Creating the directory '$DirName' ..."
 	mkdir -p "$DirName"
 	if [[ $? -eq 0 ]]; then 
-		echo "Directory '$DirName' created successfully."
+		echo "Directory '$DirName' has been created successfully."
 	else
 		"Error: could not create directory '$DirName'."
 		exit 1
@@ -19,4 +19,24 @@ else
 	echo "Error: Please provide your name and try again"
 	exit 1
 fi
+
+echo "-------------------------------------------------"
+
+#creating the sub-directories 
+echo "Creating the sub-directories ..."
+mkdir -p "$DirName/app"
+mkdir -p "$DirName/modules"
+mkdir -p "$DirName/assets"
+mkdir -p "$DirName/config"
+
+echo "sub-directories have been created successfully."
+echo "-------------------------------------------------"
+#creating the files needed
+echo "creating the files under their respective directories ..."
+touch "$DirName/app/reminder.sh"
+touch "$DirName/modules/fuctions.sh"
+touch "$DirName/assets/submissions.txt"
+touch "$DirName/config/config.env"
+
+echo "The files have been created successfully."
 
